8b02aec1b3bb2b74ba31afa74fa1b66a3570c4e9
tgunnr <finalresidentevilfantasy@gmail.com>
2024-07-23 07:02:24 -0700

    Add user prompt

--- js/app.js
+++ js/app.js
@@ -134,6 +134,15 @@ const prompts = [
           {text: 'Run Away', navigateToIdx: 3}
         ]
       },
+      {
+        id: 0,
+        question: 'Are you ready to enter?',
+        options: [
+          {text: 'Enter', navigateToIdx: 1},
+          {text: 'Leave', navigateToIdx: 2},
+          {text: 'Run Away', navigateToIdx: 3}
+        ]
+      },
   ]
   
   const questionEl = document.querySelector('#question')
