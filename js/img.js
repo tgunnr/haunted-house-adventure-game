51663f8a052755b93125e23ab294bd36372bde5c
tgunnr <finalresidentevilfantasy@gmail.com>
2024-07-26 06:42:29 -0700

    Fix all img array locations.

--- js/img.js
+++ js/img.js
@@ -1,129 +1,129 @@
 var imgArray = new Array()
 
 imgArray[0] = new Image()
-imgArray[0].src = '../assets/img/hauntedHouse.png'
+imgArray[0].src = './assets/img/hauntedHouse.png'
 
 imgArray[1] = new Image()
-imgArray[1].src = '../assets/img/enterMansion.png'
+imgArray[1].src = './assets/img/enterMansion.png'
 
 imgArray[2] = new Image()
-imgArray[2].src = '../assets/img/firstEscape.png'
+imgArray[2].src = './assets/img/firstEscape.png'
 
 imgArray[3] = new Image()
-imgArray[3].src = '../assets/img/firstCarEscape.png'
+imgArray[3].src = './assets/img/firstCarEscape.png'
 
 imgArray[4] = new Image()
-imgArray[4].src = '../assets/img/stairs.png'
+imgArray[4].src = './assets/img/stairs.png'
 
 imgArray[5] = new Image()
-imgArray[5].src = '../assets/img/library.png'
+imgArray[5].src = './assets/img/library.png'
 
 imgArray[6] = new Image()
-imgArray[6].src = '../assets/img/kitchen.png'
+imgArray[6].src = './assets/img/kitchen.png'
 
 // Stairs pathway img assets
 
 imgArray[7] = new Image()
-imgArray[7].src = '../assets/img/masterBedroom.png'
+imgArray[7].src = './assets/img/masterBedroom.png'
 
 imgArray[8] = new Image()
-imgArray[8].src = '../assets/img/guestRoom.png'
+imgArray[8].src = './assets/img/guestRoom.png'
 
 imgArray[9] = new Image()
-imgArray[9].src = '../assets/img/abyss.png'
+imgArray[9].src = './assets/img/abyss.png'
 
 // Stairs -> Bathroom img assets
 
 imgArray[10] = new Image()
-imgArray[10].src = '../assets/img/bathroom.png'
+imgArray[10].src = './assets/img/bathroom.png'
 
 imgArray[11] = new Image()
-imgArray[11].src = '../assets/img/bathtub.png'
+imgArray[11].src = './assets/img/bathtub.png'
 
 imgArray[12] = new Image()
-imgArray[12].src = '../assets/img/bathtubCreature.png'
+imgArray[12].src = './assets/img/bathtubCreature.png'
 
 imgArray[13] = new Image()
-imgArray[13].src = '../assets/img/bathtubHand.png'
+imgArray[13].src = './assets/img/bathtubHand.png'
 
 // Stairs -> Balcony img asset
 
 imgArray[14] = new Image()
-imgArray[14].src = '../assets/img/balcony.png'
+imgArray[14].src = './assets/img/balcony.png'
 
 // Stairs -> Bat img asset
 
 imgArray[15] = new Image()
-imgArray[15].src = '../assets/img/batSwirl.png'
+imgArray[15].src = './assets/img/batSwirl.png'
 
 // Stairs -> Nap img asset
 
 imgArray[16] = new Image()
-imgArray[16].src = '../assets/img/bed.png'
+imgArray[16].src = './assets/img/bed.png'
 
 imgArray[17] = new Image()
-imgArray[17].src = '../assets/img/bed.png'
+imgArray[17].src = './assets/img/bed.png'
 
 // Stairs -> Diary pathway
 
 imgArray[18] = new Image()
-imgArray[18].src = '../assets/img/masquerade.png'
+imgArray[18].src = './assets/img/masquerade.png'
 
 imgArray[19] = new Image()
-imgArray[19].src = '../assets/img/masqueradeDance.png'
+imgArray[19].src = './assets/img/masqueradeDance.png'
 
 imgArray[20] = new Image()
-imgArray[20].src = '../assets/img/masqueradeBuffet.png'
+imgArray[20].src = './assets/img/masqueradeBuffet.png'
 
 imgArray[21] = new Image()
-imgArray[21].src = '../assets/img/masqueradeDiaryOwner.png'
+imgArray[21].src = './assets/img/masqueradeDiaryOwner.png'
 
 imgArray[22] = new Image()
-imgArray[22].src = '../assets/img/authorEnding.png'
+imgArray[22].src = './assets/img/authorEnding.png'
 
 // Stairs -> Medicine Cabinet end
 
 imgArray[23] = new Image()
-imgArray[23].src = '../assets/img/medicineCabinet.png'
+imgArray[23].src = './assets/img/medicineCabinet.png'
 
 // Library img assets
 
 imgArray[24] = new Image()
-imgArray[24].src = '../assets/img/fallingBooks.png'
+imgArray[24].src = './assets/img/fallingBooks.png'
 
 // Library -> Secret Passageway pathway
 
 imgArray[25] = new Image()
-imgArray[25].src = '../assets/img/cellar.png'
+imgArray[25].src = './assets/img/cellar.png'
 
 imgArray[26] = new Image()
-imgArray[26].src = '../assets/img/cellarExorcism.png'
+imgArray[26].src = './assets/img/cellarExorcism.png'
 
 imgArray[27] = new Image()
-imgArray[27].src = '../assets/img/poisonWine.png'
+imgArray[27].src = './assets/img/poisonWine.png'
 
 imgArray[28] = new Image()
-imgArray[28].src = '../assets/img/ghostAudience.png'
+imgArray[28].src = './assets/img/ghostAudience.png'
 
 imgArray[29] = new Image()
-imgArray[29].src = '../assets/img/talkShow.png'
+imgArray[29].src = './assets/img/talkShow.png'
 
 // Kitchen pathway
 
 imgArray[30] = new Image()
-imgArray[30].src = '../assets/img/pie.png'
+imgArray[30].src = './assets/img/pie.png'
 
 imgArray[31] = new Image()
-imgArray[31].src = '../assets/img/fridge.png'
+imgArray[31].src = './assets/img/fridge.png'
 
 imgArray[32] = new Image()
 imgArray[32].src = '../assets/img/corridor.png'
 
 imgArray[33] = new Image()
-imgArray[33].src = '../assets/img/caught.png'
+imgArray[33].src = './assets/img/caught.png'
 
 imgArray[34] = new Image()
-imgArray[34].src = '../assets/img/ghostHunters.png'
+imgArray[34].src = './assets/img/ghostHunters.png'
 
 // Library -> Fireplace img
 
